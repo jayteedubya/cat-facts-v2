ae8742fc9896c3c3b3c46f8a5de80a5e36906826
jayteedubya <jayteedubya@gmail.com>
2021-03-05 16:46:35 -0800

    added root route to server

--- server/server.js
+++ server/server.js
@@ -2,3 +2,19 @@ const express = require('express');
 const db = require('./databaseHandler.js');
 const ejs = require('ejs');
 
+const PORT = process.env.PORT || 4001;
+const app = express();
+
+app.get('/', async (req, res, next) => {
+    response = await db.getAll();
+    res.send(response);
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
